0113807b55d85f6b1a36d55980fdd1a6f8e8bf8d
idk <idk>
2020-08-17 18:41:34 +0530

    branch1 commit1-basic UI

---
+++ lib/habit_name.dart
@@ -0,0 +1,2 @@
+Set<String> images={'img0.svg','img1.svg','img2.svg','img3.svg','img4.svg','img5.svg'};
+Set<String> titles={'Happiness','Exercise','No Junk Food','Study','Sleep','Positive Mindset'};
